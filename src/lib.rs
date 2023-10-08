2794f3825c401e8c0d480166ff182b16ead1821b
Cole MacKenzie <colemackenzie1@gmail.com>
2023-10-08 10:09:45 -0700

    refactor: move formatter to own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,7 @@
-use std::fmt::{self};
+mod formatter;
 
-use serde::ser::SerializeMap;
-use serde::Serializer;
-
-use tracing_core::{Event, Subscriber};
-use tracing_serde::fields::AsMap;
-use tracing_subscriber::fmt::format::{self, FormatEvent, FormatFields};
-use tracing_subscriber::fmt::{FmtContext, FormattedFields, Layer, SubscriberBuilder};
+use tracing_core::Subscriber;
+use tracing_subscriber::fmt::{Layer, SubscriberBuilder};
 use tracing_subscriber::registry::LookupSpan;
 
 /// A timestamp format for the JSON formatter.
@@ -50,66 +45,6 @@ impl TimestampFormat {
     }
 }
 
-/// A JSON formatter for tracing events.
-/// This is used to format the event field in the JSON output.
-pub struct JsonEventFormatter {
-    level_name: &'static str,
-    message_name: &'static str,
-    target_name: &'static str,
-    timestamp_name: &'static str,
-    timestamp_format: TimestampFormat,
-    flatten_fields: bool,
-}
-
-impl Default for JsonEventFormatter {
-    fn default() -> Self {
-        Self {
-            level_name: "level",
-            message_name: "message",
-            target_name: "target",
-            timestamp_name: "timestamp",
-            timestamp_format: TimestampFormat::Rfc3339,
-            flatten_fields: true,
-        }
-    }
-}
-
-impl JsonEventFormatter {
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    pub fn with_level_name(mut self, level_name: &'static str) -> Self {
-        self.level_name = level_name;
-        self
-    }
-
-    pub fn with_message_name(mut self, message_name: &'static str) -> Self {
-        self.message_name = message_name;
-        self
-    }
-
-    pub fn with_target_name(mut self, target_name: &'static str) -> Self {
-        self.target_name = target_name;
-        self
-    }
-
-    pub fn with_timestamp_name(mut self, timestamp_name: &'static str) -> Self {
-        self.timestamp_name = timestamp_name;
-        self
-    }
-
-    pub fn with_timestamp_format(mut self, timestamp_format: TimestampFormat) -> Self {
-        self.timestamp_format = timestamp_format;
-        self
-    }
-
-    pub fn with_flatten_fields(mut self, flatten_fields: bool) -> Self {
-        self.flatten_fields = flatten_fields;
-        self
-    }
-}
-
 #[derive(Debug, thiserror::Error)]
 enum Error {
     #[error("fmt error: {0}")]
@@ -125,133 +60,6 @@ enum Error {
 impl From<Error> for std::fmt::Error {
     fn from(_: Error) -> Self {
         Self
-    }
-}
-
-impl<S, N> FormatEvent<S, N> for JsonEventFormatter
-where
-    S: Subscriber + for<'a> LookupSpan<'a>,
-    N: for<'a> FormatFields<'a> + 'static,
-{
-    fn format_event(
-        &self,
-        ctx: &FmtContext<'_, S, N>,
-        mut writer: format::Writer<'_>,
-        event: &Event<'_>,
-    ) -> fmt::Result {
-        let now = chrono::Utc::now();
-
-        let mut buffer = Vec::new();
-        let mut binding = serde_json::Serializer::new(&mut buffer);
-        let mut serializer = binding.serialize_map(None).map_err(Error::Serde)?;
-
-        serializer
-            .serialize_entry(
-                self.level_name,
-                &event.metadata().level().to_string().to_lowercase(),
-            )
-            .map_err(Error::Serde)?;
-
-        if matches!(
-            self.timestamp_format,
-            TimestampFormat::Unix | TimestampFormat::UnixMillis
-        ) {
-            serializer
-                .serialize_entry(
-                    self.timestamp_name,
-                    &self.timestamp_format.format_number(&now),
-                )
-                .map_err(Error::Serde)?;
-        } else {
-            serializer
-                .serialize_entry(
-                    self.timestamp_name,
-                    &self.timestamp_format.format_string(&now),
-                )
-                .map_err(Error::Serde)?;
-        }
-
-        serializer
-            .serialize_entry(self.target_name, event.metadata().target())
-            .map_err(Error::Serde)?;
-
-        if self.flatten_fields {
-            let mut visitor = tracing_serde::SerdeMapVisitor::new(serializer);
-            event.record(&mut visitor);
-
-            serializer = visitor.take_serializer().map_err(|_| Error::Unknown)?;
-        } else {
-            serializer
-                .serialize_entry("fields", &event.field_map())
-                .map_err(Error::Serde)?;
-        };
-
-        let span = event
-            .parent()
-            .and_then(|id| ctx.span(id))
-            .or_else(|| ctx.lookup_current());
-
-        // Write all fields from spans
-        if let Some(leaf_span) = span {
-            for span in leaf_span.scope().from_root() {
-                let ext = span.extensions();
-                let data = ext
-                    .get::<FormattedFields<N>>()
-                    .expect("Unable to find FormattedFields in extensions; this is a bug");
-
-                if !data.is_empty() {
-                    let obj: Option<serde_json::Value> = serde_json::from_str(data.as_str()).ok();
-                    if matches!(obj, Some(serde_json::Value::Object(_))) {
-                        let obj = obj.expect("matched object");
-                        for (key, value) in obj.as_object().unwrap() {
-                            serializer
-                                .serialize_entry(key, value)
-                                .map_err(Error::Serde)?;
-                        }
-                    }
-                }
-            }
-        }
-
-        serializer.end().map_err(Error::Serde)?;
-        writer.write_str(std::str::from_utf8(&buffer).map_err(Error::Utf8)?)?;
-        writer.write_char('\n')?;
-
-        Ok(())
-    }
-}
-
-pub struct FieldsFormatter {}
-
-impl FieldsFormatter {
-    pub fn new() -> Self {
-        Self {}
-    }
-}
-
-impl Default for FieldsFormatter {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl<'writer> FormatFields<'writer> for FieldsFormatter {
-    fn format_fields<R>(&self, mut writer: format::Writer<'writer>, fields: R) -> fmt::Result
-    where
-        R: tracing_subscriber::field::RecordFields,
-    {
-        let mut buffer = Vec::new();
-        let mut binding = serde_json::Serializer::new(&mut buffer);
-        let mut serializer = binding.serialize_map(None).map_err(Error::Serde)?;
-        let mut visitor = tracing_serde::SerdeMapVisitor::new(serializer);
-
-        fields.record(&mut visitor);
-
-        serializer = visitor.take_serializer().map_err(|_| Error::Unknown)?;
-        serializer.end().map_err(Error::Serde)?;
-        writer.write_str(std::str::from_utf8(&buffer).map_err(Error::Utf8)?)?;
-
-        Ok(())
     }
 }
 
@@ -272,7 +80,7 @@ impl<'writer> FormatFields<'writer> for FieldsFormatter {
 ///
 /// tracing_subscriber::registry()
 ///     .with(
-///         tracing_ndjson::builder()
+///         tracing_ndjson::Builder::default()
 ///             .with_level_name("severity")
 ///             .with_message_name("msg")
 ///             .with_timestamp_name("ts")
@@ -282,15 +90,15 @@ impl<'writer> FormatFields<'writer> for FieldsFormatter {
 ///
 /// tracing::info!(life = 42, "Hello, world!");
 pub struct Builder {
-    events: JsonEventFormatter,
-    fields: FieldsFormatter,
+    events: formatter::JsonEventFormatter,
+    fields: formatter::FieldsFormatter,
 }
 
 impl Builder {
     pub fn new() -> Self {
         Self {
-            events: JsonEventFormatter::new(),
-            fields: FieldsFormatter::new(),
+            events: formatter::JsonEventFormatter::new(),
+            fields: formatter::FieldsFormatter::new(),
         }
     }
 }
@@ -301,42 +109,57 @@ impl Default for Builder {
     }
 }
 
+/// Alias for `Builder::default()`.
+/// This is used to configure the JSON formatter.
 pub fn builder() -> Builder {
     Builder::default()
 }
 
 impl Builder {
+    /// Set the field name for the level field.
+    /// The default is "level".
     pub fn with_level_name(mut self, level_name: &'static str) -> Self {
         self.events = self.events.with_level_name(level_name);
         self
     }
 
+    /// Set the field name for the message field.
+    /// The default is "message".
     pub fn with_message_name(mut self, message_name: &'static str) -> Self {
         self.events = self.events.with_message_name(message_name);
         self
     }
 
+    /// Set the field name for the target field.
+    /// The default is "target".
     pub fn with_target_name(mut self, target_name: &'static str) -> Self {
         self.events = self.events.with_target_name(target_name);
         self
     }
 
+    /// Set the field name for the timestamp field.
+    /// The default is "timestamp".
     pub fn with_timestamp_name(mut self, timestamp_name: &'static str) -> Self {
         self.events = self.events.with_timestamp_name(timestamp_name);
         self
     }
 
+    /// Set the timestamp format for the timestamp field.
+    /// The default is TimestampFormat::Rfc3339.
     pub fn with_timestamp_format(mut self, timestamp_format: TimestampFormat) -> Self {
         self.events = self.events.with_timestamp_format(timestamp_format);
         self
     }
 
+    /// Set whether to flatten fields.
+    /// The default is true. If false, fields will be nested under a "fields" object.
     pub fn with_flatten_fields(mut self, flatten_fields: bool) -> Self {
         self.events = self.events.with_flatten_fields(flatten_fields);
         self
     }
 
-    pub fn layer<S>(self) -> Layer<S, FieldsFormatter, JsonEventFormatter>
+    /// Return a `Layer` that subscribes to all spans and events using the defined formatter.
+    pub fn layer<S>(self) -> Layer<S, formatter::FieldsFormatter, formatter::JsonEventFormatter>
     where
         S: Subscriber + for<'a> LookupSpan<'a>,
     {
@@ -345,7 +168,9 @@ impl Builder {
             .fmt_fields(self.fields)
     }
 
-    pub fn subscriber_builder(self) -> SubscriberBuilder<FieldsFormatter, JsonEventFormatter> {
+    pub fn subscriber_builder(
+        self,
+    ) -> SubscriberBuilder<formatter::FieldsFormatter, formatter::JsonEventFormatter> {
         tracing_subscriber::fmt::Subscriber::builder()
             .event_format(self.events)
             .fmt_fields(self.fields)
@@ -370,9 +195,9 @@ mod tests {
 
     #[test]
     fn test_json_event_formatter() {
-        let formatter = JsonEventFormatter::new();
+        let formatter = formatter::JsonEventFormatter::new();
         let subscriber = tracing_subscriber::fmt()
-            .fmt_fields(FieldsFormatter::new())
+            .fmt_fields(formatter::FieldsFormatter::new())
             .event_format(formatter)
             .with_max_level(Level::TRACE)
             .finish();
@@ -390,14 +215,14 @@ mod tests {
             });
         });
 
-        let formatter = JsonEventFormatter::new()
+        let formatter = formatter::JsonEventFormatter::new()
             .with_level_name("severity")
             .with_message_name("msg")
             .with_timestamp_name("ts")
             .with_timestamp_format(TimestampFormat::Unix);
 
         let subscriber = tracing_subscriber::fmt()
-            .fmt_fields(FieldsFormatter::new())
+            .fmt_fields(formatter::FieldsFormatter::new())
             .event_format(formatter)
             .with_max_level(Level::TRACE)
             .finish();
@@ -415,6 +240,10 @@ mod tests {
             );
             span.in_scope(|| {
                 info!("some message from inside a info_span");
+                let inner = info_span!("inner_span", a = "b", c = "d");
+                inner.in_scope(|| {
+                    info!("some message from inside a info_span");
+                });
             });
         });
     }
